3482db6ea87bc60331cdc8a48d592bd9f8819b64
geraltofgermania <frankfernandezavila@gmail.com>
2023-08-05 09:29:06 -0700

    outline and main file created plus basic structure

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from colorama import Fore
+
+def home():
+    pass
+
+def main():
+    print(f"{Fore.BLUE}To Do List\n")
+
+    #reset terminal colors
+    print(Fore.RESET)
+if __name__ == "__main__":
+    main()
